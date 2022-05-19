e177abb9caa103e0cc53a61265b63942e3bd6d00
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-19 09:18:26 +0200

    avanzando con los menus

--- js/main.js
+++ js/main.js
@@ -71,4 +71,26 @@ $(document).ready(() => {
     );
     return false;
   });
+
+  //login falso
+
+  $("#login form").submit(function () {
+    var form_name = $("#form_name").val();
+    localStorage.setItem("form_name", form_name);
+  });
+
+  var form_name = localStorage.getItem("form_name");
+
+  if (form_name != null && form_name != "undefined") {
+    var about_parrafo = $("#about p");
+    about_parrafo.html("<strong>bienvenido," + form_name + "</strong>");
+    about_parrafo.append("<a href='#' id='logout'>Cerrar sesión</a>");
+
+    $("#login").hide();
+
+    $("logout").click(function () {
+      localStorage.clear();
+      location.reload();
+    });
+  }
 });
